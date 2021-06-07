35560e55667f99b9049a789cc11ba737ccc1abc5
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 23:20:01 +0200

    sfsf
    dfvb

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const discord = require("discord.js");
-const botConfig = require("./botconfig");
- 
-const client = new discord.Client();
-client.login(process.env.token);
- 
-client.on("ready", async () => {
- 
-    console.log(`${client.user.username} is online.`);
-    client.user.setActivity("Tim", { type: "PLAYING" });
- 
-});
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.db = require("quick.db");
+client.request = new (require("rss-parser"))();
+client.config = require("./config.js");
 
-handleUploads();
+client.on("ready", () => {
+    console.log("I'm ready!");
+    handleUploads();
+});
 
 function handleUploads() {
     if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
@@ -35,19 +31,4 @@ function handleUploads() {
     }, client.config.watchInterval);
 }
 
-client.on("message", async message =>{
-
-    if(message.author.bot) return;
-
-    if(message.channel.type == "dm") return;
-
-    var prefix = botConfig.prefix;
-
-    var messageArray = message.content.split(" ");
-
-    var command = messageArray[0];
-
-    if(command === `${prefix}hallo`){
-        return message.channel.send("Hallo!");
-    }
-});
+client.login(process.env.token);
