c4d405acf3c3b977d053b30c6d58120b1bc289b3
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 23:27:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,37 @@ client.on("message", async message =>{
         return message.channel.send("Hallo!");
     }
 });
+
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}twitch`){
+        return message.channel.send("TWITCH!");
+    }
+});
+
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}yt`){
+        return message.channel.send("YOUTUBE!!");
+    }
+});
