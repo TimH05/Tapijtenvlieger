3730d3e1033f94b61bd9d538f0bd595e52865587
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 22:55:32 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,7 +1,7 @@
 {
-    channel: "XXXXXXXXXXXXXXXXXX",
+    channel: "800484769360117763",
     messageTemplate; "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id; "YOUTUBE_CHANNEL_ID",
+    channel_id; "UCO5E6Vrq4-GjfIeYag9uEnA",
     watchInterval; 30000
     ,"prefix";"-"
 }
