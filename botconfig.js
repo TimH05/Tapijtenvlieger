ffeeaec5bc33bd08623a885bf9205d3db5dffeab
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 22:35:09 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     channel: "800484769360117763", // channel id of a channel to send message
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}", // message to send on discord
-    channel_id: "UCO5E6Vrq4-GjfIeYag9uEnA", // youtube channel id
+    channel_id: "O5E6Vrq4-GjfIeYag9uEnA", // youtube channel id
     watchInterval: 30000 // Check every 30 seconds
 
     ,"prefix":"-"
