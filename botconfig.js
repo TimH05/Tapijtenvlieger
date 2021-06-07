88fe10af762e4dfbbf8a00bff80a05b2fae4c06d
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 22:53:41 +0200

    12121

---
+++ botconfig.js
@@ -0,0 +1,7 @@
+{
+    channel: "XXXXXXXXXXXXXXXXXX",
+    messageTemplate; "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+    channel_id; "YOUTUBE_CHANNEL_ID",
+    watchInterval; 30000
+    ,"prefix";"-"
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const discord = require("discord.js");
-const botConfig = require("./botconfig.json");
+const botConfig = require("./botconfig");
  
 const client = new discord.Client();
 client.login(process.env.token);
@@ -10,6 +10,30 @@ client.on("ready", async () => {
     client.user.setActivity("Tim", { type: "PLAYING" });
  
 });
+
+handleUploads();
+
+function handleUploads() {
+    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
+    setInterval(() => {
+        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+        .then(data => {
+            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+            else {
+                client.db.set(`videoData`, data.items[0]);
+                client.db.push("postedVideos", data.items[0].link);
+                let parsed = client.db.fetch(`videoData`);
+                let channel = client.channels.cache.get(client.config.channel);
+                if (!channel) return;
+                let message = client.config.messageTemplate
+                    .replace(/{author}/g, parsed.author)
+                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                    .replace(/{url}/g, parsed.link);
+                channel.send(message);
+            }
+        });
+    }, client.config.watchInterval);
+}
 
 client.on("message", async message =>{
 
