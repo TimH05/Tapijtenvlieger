1de5c34281a4bd9170096aa03bb785e2f8f230f2
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 22:50:03 +0200

    1

--- botconfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    channel: "800484769360117763", // channel id of a channel to send message
-    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}", // message to send on discord
-    channel_id: "O5E6Vrq4-GjfIeYag9uEnA", // youtube channel id
-    watchInterval: 30000 // Check every 30 seconds
-
-    ,"prefix":"-"
-};
