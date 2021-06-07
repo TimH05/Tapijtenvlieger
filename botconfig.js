a7eab78ecde5bf18a93146b7464ee12ad056d509
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 23:23:48 +0200

    Init

--- botconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    channel: "800484769360117763",
-    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "UCO5E6Vrq4-GjfIeYag9uEnA",
-    watchInterval: 30000
-}
--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.db = require("quick.db");
-client.request = new (require("rss-parser"))();
-client.config = require("./config.js");
-
-client.on("ready", () => {
-    console.log("I'm ready!");
-    handleUploads();
+const discord = require("discord.js");
+const botConfig = require("./botconfig");
+ 
+const client = new discord.Client();
+client.login(process.env.token);
+ 
+client.on("ready", async () => {
+ 
+    console.log(`${client.user.username} is online.`);
+    client.user.setActivity("Tim", { type: "PLAYING" });
+ 
 });
 
-function handleUploads() {
-    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
-    setInterval(() => {
-        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
-        .then(data => {
-            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
-            else {
-                client.db.set(`videoData`, data.items[0]);
-                client.db.push("postedVideos", data.items[0].link);
-                let parsed = client.db.fetch(`videoData`);
-                let channel = client.channels.cache.get(client.config.channel);
-                if (!channel) return;
-                let message = client.config.messageTemplate
-                    .replace(/{author}/g, parsed.author)
-                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
-                    .replace(/{url}/g, parsed.link);
-                channel.send(message);
-            }
-        });
-    }, client.config.watchInterval);
-}
 
-client.login(process.env.token);
+
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}hallo`){
+        return message.channel.send("Hallo!");
+    }
+});
