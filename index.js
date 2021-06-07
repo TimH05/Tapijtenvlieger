a29e2986c776d539557f8c2a99d6ebe7d3b2800d
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-07 18:44:48 +0200

    Init
    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+ 
+const client = new discord.Client();
+client.login(process.env.token);
+ 
+client.on("ready", async () => {
+ 
+    console.log(`${client.user.username} is online.`);
+    client.user.setActivity("Tim", { type: "PLAYING" });
+ 
+});
+
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}hallo`){
+        return message.channel.send("Hallo!");
+    }
+});
