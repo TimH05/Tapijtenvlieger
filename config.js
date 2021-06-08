23bdacd5346a315552a0a5f3d2f59ae2dac4a3b6
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-08 19:49:51 +0200

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    channel: "800484769360117763",
+    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
+    channel_id: "UCO5E6Vrq4-GjfIeYag9uEnA",
+    watchInterval: 30000,
+
+    "prefix":"-"
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const discord = require("discord.js");
-const botConfig = require("./botconfig");
+const botConfig = require("./config.js");
  
 const client = new discord.Client();
 client.login(process.env.token);
@@ -8,10 +8,30 @@ client.on("ready", async () => {
  
     console.log(`${client.user.username} is online.`);
     client.user.setActivity("TapijtenVlieger", { type: "WATCHING" });
- 
+    handleUploads();
 });
 
-
+function handleUploads() {
+    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
+    setInterval(() => {
+        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+        .then(data => {
+            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+            else {
+                client.db.set(`videoData`, data.items[0]);
+                client.db.push("postedVideos", data.items[0].link);
+                let parsed = client.db.fetch(`videoData`);
+                let channel = client.channels.cache.get(client.config.channel);
+                if (!channel) return;
+                let message = client.config.messageTemplate
+                    .replace(/{author}/g, parsed.author)
+                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                    .replace(/{url}/g, parsed.link);
+                channel.send(message);
+            }
+        });
+    }, client.config.watchInterval);
+}
 
 client.on("message", async message =>{
 
@@ -27,22 +47,5 @@ client.on("message", async message =>{
 
     if(command === `${prefix}hallo`){
         return message.channel.send("Hallo!");
-    }
-});
-
-client.on("message", async message =>{
-
-    if(message.author.bot) return;
-
-    if(message.channel.type == "dm") return;
-
-    var prefix = botConfig.prefix;
-
-    var messageArray = message.content.split(" ");
-
-    var command = messageArray[0];
-
-    if(command === `${prefix}youtube`){
-        return message.channel.send("HEY @everyone !!! Tapijtenvlieger heeft een nieuwe video geupload! https://www.youtube.com/");
     }
 });
