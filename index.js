c7bd8e26db45e17493bf9a04ad6cbd4e939de176
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-30 20:52:18 +0200

    1

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ client.on("ready", async () => {
     handleUploads();
 });
 
+function handleUploads() {
+    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
+    setInterval(() => {
+        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
+        .then(data => {
+            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
+            else {
+                client.db.set(`videoData`, data.items[0]);
+                client.db.push("postedVideos", data.items[0].link);
+                let parsed = client.db.fetch(`videoData`);
+                let channel = client.channels.cache.get(client.config.channel);
+                if (!channel) return;
+                let message = client.config.messageTemplate
+                    .replace(/{author}/g, parsed.author)
+                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
+                    .replace(/{url}/g, parsed.link);
+                channel.send(message);
+            }
+        });
+    }, client.config.watchInterval);
+}
+
 client.on("message", async message =>{
 
     if(message.author.bot) return;
@@ -23,8 +45,8 @@ client.on("message", async message =>{
 
     var command = messageArray[0];
 
-    if(command === `${prefix}online?`){
-        return message.channel.send("Ik ben online!");
+    if(command === `${prefix}hallo`){
+        return message.channel.send("Hallo!");
     }
 });
 
