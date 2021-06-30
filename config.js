9242a3ee0e57b7d1b6b2908c7b88265b79186753
TimH05 <85509735+TimH05@users.noreply.github.com>
2021-06-30 20:45:11 +0200

    Yea

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-module.exports = {
-    channel: "800484769360117763",
-    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "UCO5E6Vrq4-GjfIeYag9uEnA",
-    watchInterval: 30000,
-
-    "prefix":"-"
-}
+"prefix";"-"
--- index.js
+++ index.js
@@ -11,27 +11,22 @@ client.on("ready", async () => {
     handleUploads();
 });
 
-function handleUploads() {
-    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
-    setInterval(() => {
-        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.channel_id}`)
-        .then(data => {
-            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
-            else {
-                client.db.set(`videoData`, data.items[0]);
-                client.db.push("postedVideos", data.items[0].link);
-                let parsed = client.db.fetch(`videoData`);
-                let channel = client.channels.cache.get(client.config.channel);
-                if (!channel) return;
-                let message = client.config.messageTemplate
-                    .replace(/{author}/g, parsed.author)
-                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
-                    .replace(/{url}/g, parsed.link);
-                channel.send(message);
-            }
-        });
-    }, client.config.watchInterval);
-}
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}online?`){
+        return message.channel.send("Ik ben online!");
+    }
+});
 
 client.on("message", async message =>{
 
@@ -45,7 +40,24 @@ client.on("message", async message =>{
 
     var command = messageArray[0];
 
-    if(command === `${prefix}hallo`){
-        return message.channel.send("Hallo!");
+    if(command === `${prefix}youtube`){
+        return message.channel.send("@everyone - TapijtenVlieger heeft een nieuwe video geupload! Check hem snel op zijn kanaal! https://www.youtube.com/channel/UC-bTYzNFaCGdlxVVhJiIPCg");
+    }
+});
+
+client.on("message", async message =>{
+
+    if(message.author.bot) return;
+
+    if(message.channel.type == "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    if(command === `${prefix}twitch`){
+        return message.channel.send("@everyone - TapijtenVlieger is live op Twitch! Ga snel kijken!! https://www.twitch.tv/tapijtenvlieger");
     }
 });
